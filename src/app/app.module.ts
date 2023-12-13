4b9d6efd1e5da7767e6fd6927cfb98ad9c2da9f7
Rudresh Patidar <rudresh.patidar@capgemini.com>
2023-12-13 09:47:41 +0530

    Project Skeleton is Created.

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CountriesComponent } from './components/countries/countries.component';
+import { SalesComponent } from './components/sales/sales.component';
+import { CostsComponent } from './components/costs/costs.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { ChannelsComponent } from './components/channels/channels.component';
+import { TimesComponent } from './components/times/times.component';
+import { PromotionsComponent } from './components/promotions/promotions.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomersComponent,
+    ProductsComponent,
+    CountriesComponent,
+    SalesComponent,
+    CostsComponent,
+    ProductsComponent,
+    CustomersComponent,
+    ChannelsComponent,
+    TimesComponent,
+    PromotionsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/models/channels-model.ts
@@ -0,0 +1,10 @@
+export class ChannelsModel {
+    constructor(
+        private channelId:number,
+        private channelDesc:string,
+        private channelClass:string,
+        private channelClassId:number,
+        private channelTotal:string,
+        private channelTotalId:number,
+    ){}
+}
